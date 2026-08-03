26983ed3ff430598704f2e8858f5bd8111d8a3e0
teikk <nguyenkiet228@gmail.com>
2026-08-03 22:32:32 +0700

    One2Many relationship with typeORM

--- src/entities/property.entity.ts
+++ src/entities/property.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { PropertyFeature } from "./property.feature.entity";
+import { User } from "./user.entity";
 
 @Entity()
 export class Property {
@@ -24,8 +25,14 @@ export class Property {
         // cascade: ["remove"] mean if you delete property, it will delete propertyFeature automatically
         // cascade: ["insert", "update", "remove"] mean if you insert, update or delete property, it will insert, update or delete propertyFeature automatically
         // cascade: [] mean if you update property, it will not update propertyFeature automatically (this is default) like a false
-
         { cascade: true }
     )
     propertyFeature: PropertyFeature;
+
+    // One to Many relationship with User Entity
+    // That mean one user can have many properties
+    @ManyToOne(() => User, (user) => user.properties)
+    // Change default column name of foreign key from userId to owner_id
+    @JoinColumn({ name: "owner_id" })
+    user: User;
 }
